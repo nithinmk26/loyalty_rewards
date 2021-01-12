9978186b125f892e4aa6cf61b4970308eeb24071
NITHIN M K <nithinmk26@gmail.com>
2021-01-12 21:34:05 +0530

    you can push noe

--- product-service/src/main/java/com/digital/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/digital/productservice/controller/ProductController.java
@@ -1,52 +1,18 @@
 package com.digital.productservice.controller;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.digital.productservice.entity.Product;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
 	
-	@GetMapping("/")
-	public List<Product> getAllProducts(){
-		return null;
-	}
-	
-	@GetMapping("/2")
-	public List<Product> getAllProducts2(){
-		return null;
-	}
-	
-	@GetMapping("/3")
-	public List<Product> getAllProducts3(){
-		return null;
+	@GetMapping("/home")
+	public String getAllProducts(){
+		return " Welcome to home Screen";
 	}
 
-	@GetMapping("/fromnithin1")
-	public List<Product> getAllProductsFromNithin(){
-		return null;
-	}
-	
-	@GetMapping("/fromNithin")
-	public List<Product> getAllProductsFromKoms(){
-		return null;
-	}
-	
-	@GetMapping("/fromKoms")
-	public List<Product> getAllProductsFromKoms1(){
-		return null;
-	}
-	
-	@GetMapping("/form")
-	public List<Product> get(){
-		return null;
-	}
-		
 	
 }
