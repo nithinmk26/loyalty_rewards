bf8e6275fe0159e4ec915c0bef28c41f377c6760
komala8890 <komikomala17@gmail.com>
2021-01-15 23:52:09 +0530

    final done

--- cart-service/src/main/java/com/digital/cart/repository/ItemDetailsRepository.java
+++ cart-service/src/main/java/com/digital/cart/repository/ItemDetailsRepository.java
@@ -1,6 +1,7 @@
 package com.digital.cart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.digital.cart.entity.Item;
